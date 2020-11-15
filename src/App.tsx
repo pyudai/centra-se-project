41132cec552ec0f8296b8be1cc88ecf9e897177f
pyudai <premyuda321@gmail.com>
2020-11-15 21:47:01 +0700

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Check from "./pages/Checkin-out";
 import Employee from "./pages/Employee";
-import Promotion from "./pages/Promotion";
 import Reserve from "./pages/Reserve";
+import Promotion from './pages/Promotion';
 
 function App() {
   return (
     <div className="App font-prompt bg-blue-200">
       <Router>
         <Switch>
-          <Route exact path="/" component={Login} />
           <Route exact path="/Checkin-out" component={Check} />
           <Route exact path="/Employee" component={Employee} />
           <Route exact path="/Promotion" component={Promotion} />
           <Route exact path="/Reserve" component={Reserve} />
+          <Route exact path="/" component={Login} />
         </Switch>
       </Router>
     </div>
