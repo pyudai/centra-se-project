6a65fee4f1e01a08cee98fc7005173807e763079
peach_chanon <chanonpanarong@live.com>
2020-11-16 23:54:48 +0700

    พีท งานสำเร็จ วันที่ 16/11

--- src/pages/Login-Admin.tsx
+++ src/pages/Login-Admin.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 
 function LoginAdmin() {
   return (
-    <div>
-      <title>Login</title>
-      <img src="./img-login/Centra Resort-Logo-White.png"/>
-      <p>For Administrator</p>
+    <div className="h-screen bg-login-admin object-cover flex flex-col items-center justify-center">
+      <title>Login | Centra Resort</title>
+      <div className="border-b-2 border-white pb-8">
+        <img src="./img-login/Centra Resort-Logo-White.png" style={{maxWidth : '500px'}}/>
+      </div>
+      <div className="flex items-center text-white mt-3">
+        <img src="./img-login/login-2.png"/>
+        <p className="m-0 text-xl">For Administrator</p>
+      </div>
       <p>Login</p>
       <input type="text" name="input_username"/>
       <input type="password" name="input_password"/>
+      <div>
+        <button>Sign in</button>
+      </div>
+      <div>
+        <a>Forgot Username/ Password?</a>
+      </div>
     </div>
   );
 }
