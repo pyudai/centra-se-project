61b0d7df1fd78d1fc08fae8cf66e54cafc10fd74
peach_chanon <chanonpanarong@live.com>
2020-11-16 13:26:11 +0700

    พีท อัพเดตงาน

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import Navbar from './navbar';
 function App() {
   return (
     <div className="App font-prompt bg-blue-200">
-      <Navbar />
-      เปรมยุดาแบก SE นะจ๊ะ
-      ทุกคนไปกินข้าวเย็นที่สวนหมากกัน
-      วินนี่ หิวข้าววววว
+      <Navbar />  
     </div>
   );
 }
