d09724f9d836843527b627aceebed6ae9f65ed7b
apichaya be <maplebe1994@outlook.co.th>
2020-11-18 15:30:27 +0000

    Update Promotion.tsx

--- src/pages/Promotion.tsx
+++ src/pages/Promotion.tsx
@@ -9,22 +9,22 @@ import { Form,Upload } from "antd";
 
 
 function Promotion() {
-  return (
+    return (
     <div>
       <BeforeNav />
       <Navbar />
       <div className="flex flex-col items-center">
         <div className="h-full border-2 border-gray-200 rounded-lg p-12 my-20 shadow-lg"
           style={{ width: "1000px" }}>
-        
-            <div className="flex text-lg ">
-              <img src="pic/cutlery 2.png" alt="fork" className="w-12 h-12" />
-              <p className="font-prompt text-4xl font-bold">เพิ่มเมนู</p>
-             
-              <div className="h-full border-2 border-gray-200 rounded-lg -mx-40 flex flex-row my-24 flex flex-col p-32 shadow-lg "
-                style={{ width: "350px" }}
-              >
-                 <div className="inline-flex">
+
+          <div className="flex text-lg ">
+            <img src="pic/cutlery 2.png" alt="fork" className="w-12 h-12" />
+            <p className="font-prompt text-4xl font-bold">เพิ่มเมนู</p>
+
+            <div className="h-full border-2 border-gray-200 rounded-lg -mx-40 flex flex-row my-24 flex flex-col p-32 shadow-lg "
+              style={{ width: "350px" }}
+            >
+              <div className="inline-flex">
                 <Form>
                   <Form.Item>
                     <Upload showUploadList listType="picture-card">
@@ -38,7 +38,7 @@ function Promotion() {
                 </Form>
               </div>
             </div>
-            <div className="mx-auto flex flex-row my-24 flex flex-col">
+            <div className="mx-auto flex my-24 flex-col">
               <p className="font-prompt">ชื่อเมนู</p>
               <input type="text" id="name" name="name" className="w-full bg-gray-100 my-4 flex flex-col rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out font-prompt shadow-lg" />
               <p className="font-prompt ">รหัสอาหาร</p>
@@ -49,8 +49,11 @@ function Promotion() {
               <input type="text" id="name" name="name" className="w-64 bg-gray-100 my-4 flex flex-col rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out font-prompt shadow-lg" />
             </div>
           </div>
-          <button className="bg-nav text-white active:bg-nav font-bold my-4 flex flex-col text-sm px-16 py-3 rounded shadow hover:bg-blue-500 outline-none focus:outline-none mb-1">Back</button>
-          <Modal />
+ 
+          <div className="flex justify-between">
+            <button className="bg-nav text-white active:bg-nav font-bold text-sm px-16 py-3 rounded shadow hover:bg-blue-500 outline-none focus:outline-none mb-1">Back</button>
+            <Modal />
+          </div>
         </div>
       </div >
     </div>
