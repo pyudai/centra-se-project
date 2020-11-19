ac4c919d1d3569d2faeb078e3254ddca6d8e5fee
pyudai <premyuda321@gmail.com>
2020-11-20 01:02:20 +0700

    update room page & navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ChooseLogin from "./pages/ChooseLogin";
 import LoginAdmin from "./pages/Login-Admin";
 import LoginStaff from "./pages/Login-Staff";
-import Check from "./pages/Checkin-out";
+import Room from "./pages/Room";
 import Employee from "./pages/Employee";
 import Reserve from "./pages/Reserve";
 import Promotion from './pages/Promotion';
@@ -15,7 +15,7 @@ function App() {
     <div className="font-prompt">
       <Router>
         <Switch>
-          <Route exact path="/Checkin-out" component={Check} />
+          <Route exact path="/Room" component={Room} />
           <Route exact path="/Employee" component={Employee} />
           <Route exact path="/Promotion" component={Promotion} />
           <Route exact path="/Reserve" component={Reserve} />
