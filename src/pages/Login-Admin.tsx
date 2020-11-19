b65c5b5d36b7e0b088c4b4fe8e79aee693698d84
peach_chanon <chanonpanarong@live.com>
2020-11-19 21:58:53 +0700

    เพิ่มเงา

--- src/pages/Login-Admin.tsx
+++ src/pages/Login-Admin.tsx
@@ -16,14 +16,14 @@ function LoginAdmin() {
         </div>
         <div className="flex flex-col">
           <p className="text-2xl text-white">Login</p>
-          <input type="text" name="input_username" title="Enter username" placeholder="Enter username" className="p-3 pl-12 m-2 text-base rounded max-w-full logo-login-admin-username" required/>
-          <input type="password" name="input_password" title="Enter password" placeholder="Enter password" className="p-3 pl-12 m-2 text-base rounded max-w-full logo-login-admin-password" required/>
+          <input type="text" name="input_username" title="Enter username" placeholder="Enter username" className="p-3 pl-12 m-2 text-base rounded max-w-full logo-login-admin-username shadow-lg" required/>
+          <input type="password" name="input_password" title="Enter password" placeholder="Enter password" className="p-3 pl-12 m-2 text-base rounded max-w-full logo-login-admin-password shadow-lg" required/>
           <div className="flex m-2">
             <Checkbox className="text-base text-white">Show password</Checkbox>
           </div>
         </div>
         <div className="m-2">
-          <button className="bg-blue-600 hover:bg-blue-700 text-white p-3 text-base rounded w-full">Sign in</button>
+          <button className="bg-blue-600 hover:bg-blue-700 text-white p-3 text-base rounded w-full shadow-lg">Sign in</button>
         </div>
         <a className="hover:text-blue-700 hover:underline text-base text-white">Forgot Username/ Password?</a>
       </div>
