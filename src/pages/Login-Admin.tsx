e743e28f327d0c84775b6bee692a8a723a19f642
peach_chanon <chanonpanarong@live.com>
2020-11-19 17:58:01 +0700

    login admin interface เสร็จแล้ว

--- src/pages/Login-Admin.tsx
+++ src/pages/Login-Admin.tsx
@@ -1,38 +1,36 @@
-import React from 'react';
+import { Checkbox, Modal } from 'antd';
+import React, { useState } from 'react';
 import "../style/theme.css";
 
 function LoginAdmin() {
   return (
     <div className="h-screen bg-login-admin object-cover flex flex-col items-center justify-center">
       <title>Login | Centra Resort</title>
-      <div className="border-b-2 border-white pb-3">
-        <img src="./img-login/Centra Resort-Logo-White.png" style={{maxWidth : '400px'}}/>
-      </div>
-      <div className="flex items-center text-white mt-3">
-        <img src="./img-login/login-3.png"/>
-        <div className="ml-2">
-          <p className="m-0 text-xl">For Administrator</p>
+      <div className = "max-width-login text-center">
+        <div className="border-b-2 border-white pb-3">
+          <img src="./img-login/Centra Resort-Logo-White.png"/>
         </div>
-      </div>
-        <div className="mt-1 mb-1">
-          <p className="m-0 text-xl text-white">Login</p>
+        <div className="flex p-2 justify-center" >
+            <img src="./img-login/logo-1.svg" className="p-1"/>
+            <p className="m-0 text-xl text-white p-1">For Administrator</p>
         </div>
-        <div className="mt-1 mb-1">
-          <p className="m-0 text-white text-xl">Username :</p>
-          <input type="text" name="input_username" placeholder=" Enter username"  className="text-base p-2 rounded username" required/>
+        <div className="flex flex-col">
+          <p className="m-0 text-2xl text-white">Login</p>
+          <input type="text" name="input_username" title="Enter username" placeholder="Enter username" className="p-3 pl-12 m-2 text-base rounded max-w-full logo-login-admin-username" required/>
+          <input type="password" name="input_password" title="Enter password" placeholder="Enter password" className="p-3 pl-12 m-2 text-base rounded max-w-full logo-login-admin-password" required/>
+          <div className="flex m-2">
+            <Checkbox className="text-base text-white">Show password</Checkbox>
+          </div>
         </div>
-        <div className="mt-1 mb-1">
-          <p className="m-0 text-white text-xl">Password :</p>
-          <input type="password" name="input_password" placeholder=" Enter password"  className="text-base  p-2 rounded" required/>
+        <div className="m-2">
+          <button className="bg-blue-600 hover:bg-blue-700 text-white p-3 text-base rounded w-full">Sign in</button>
         </div>
-      <div className="mt-2 mb-1">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-20 text-xl rounded w-200px">Sign in</button>
-      </div>
-      <div className="mt-1 mb-1">
-        <a className="hover:underline">Forgot Username/ Password?</a>
+        <a className="hover:underline text-base text-white">Forgot Username/ Password?</a>
       </div>
     </div>
   );
 }
+
+
 
 export default LoginAdmin;
