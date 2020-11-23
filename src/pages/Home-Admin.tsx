d625b23486e03501b3b797584e2d20129f982187
pyudai <premyuda321@gmail.com>
2020-11-24 00:03:50 +0700

    add home & edit BeforeNav

---
+++ src/pages/Home-Admin.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BeforeNav from '../BeforeNav';
+import Navbar from '../navbar';
+import Modal from '../popup';
+import Phone from '../phone'
+import Citizen from '../citizen';
+
+function HomeA() {
+  return (
+    <div className="">
+      <title>Home</title>
+      <BeforeNav/>
+      <div className="sticky top-0 z-50"><Navbar/></div> 
+      homeAdmin
+    </div>
+  );
+}
+
+export default HomeA;
---
+++ src/pages/Home-Staff.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BNav from '../BNav-staff';
+import NavS from '../navbar-staff';
+
+
+function HomeS() {
+  
+  return (
+    <div className="">
+      <title>Home</title> 
+      <BNav/>
+      <div className="sticky top-0 z-50"><NavS/></div>
+      HomeStaff
+    </div>
+  );
+}
+
+export default HomeS;
+
