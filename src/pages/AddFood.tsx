8fe4b35f3e7fe5ee0f41eb6123e9a91a11f2ee80
peach_chanon <chanonpanarong@live.com>
2020-12-06 15:44:08 +0700

    addfood ลืมเช็คพวกกรอกข้อมูลไม่ครบ ตอนนี้ทำครบหละๆ

--- src/pages/AddFood.tsx
+++ src/pages/AddFood.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import 'antd/dist/antd.css';
 import '../style/output.css'
 import Navbar from '../navbar-admin';
@@ -11,6 +11,15 @@ function AddFood() {
 
   // Pop-up สำเร็จ
   const [state, stateComplete] = useState(false);
+
+  // Pop-up ข้อมูลไม่ครบ 
+  const [state_error, stateErrorInputData] = useState(false);
+
+  // Checkdata input
+  const [name, nameFood]  = useState("");
+  const [id, idFood]  = useState("");
+  const [detail, detailFood]  = useState("");
+  const [cost, costFood]  = useState("");
 
   let dataSet = {
     idFood: "0010020031"
@@ -41,35 +50,70 @@ function AddFood() {
             </div>
             <div className="w-1/2 flex flex-col">
               <p className="m-1 text-base font-semibold">ชื่อเมนู</p>
-              <input type="text" placeholder="กรอกชื่ออาหาร" className="m-1 text-base p-3 font-prompt shadow-lg" />
+              <input type="text" placeholder="กรอกชื่ออาหาร" className="m-1 text-base p-3 font-prompt shadow-lg" 
+                onChange={(e)=>{
+                  nameFood(e.target.value);
+                }}
+              />
               <p className="m-1 text-base font-semibold">รหัสอาหาร</p>
               <input type="number" value={dataSet.idFood}
                 onChange={(e) => {
+                  idFood(e.target.value);
                   e.target.disabled = true;
                 }}
                 name="name" className="m-1 text-base p-3 font-prompt shadow-lg bg-gray-400" />
               <p className="m-1 text-base font-semibold">รายละเอียดอาหาร</p>
-              <textarea className="m-1 text-base p-3 font-prompt shadow-lg" placeholder="กรอกรายละเอียดอาหาร">
+              <textarea className="m-1 text-base p-3 font-prompt shadow-lg" placeholder="กรอกรายละเอียดอาหาร"
+                onChange={(e)=>{
+                  detailFood(e.target.value);
+                }}
+              >
               </textarea>
               <p className="m-1 text-base font-semibold">ราคา</p>
-              <input type="number" placeholder="กรอกราคา (บาท)" className="m-1 text-base p-3 font-prompt shadow-lg" />
+              <input type="number" placeholder="กรอกราคา (บาท)" className="m-1 text-base p-3 font-prompt shadow-lg" 
+                onChange={(e)=>{
+                  costFood(e.target.value)
+                }}
+              />
             </div>
           </div>
           <div className="w-full flex items-center justify-between">
             <Link className="w-full" to="Food">
-              <button className="m-3 bg-blue-600 hover:bg-blue-700 text-white p-3 text-base rounded w-1/4 shadow-lg">Back</button>
+              <button className="m-3 bg-blue-600 hover:bg-blue-700 text-white p-3 text-base rounded w-1/4 shadow-lg"
+                onClick={()=>{message.info('ยกเลิกเพิ่มอาหาร');}}
+              >Back</button>
             </Link>
             <button className="m-3 bg-green-600 hover:bg-green-700 text-white p-3 text-base rounded w-1/4 shadow-lg"
               onClick={
                 () => {
-                  message.success('เพิ่มอาหารสำเร็จ');
-                  stateComplete(true);
+                  if(name=="" && id=="" && detail=="" && cost=="") message.warning('กรุณากรอกข้อมูลให้ครบ');
+                  else {
+                    message.success('เพิ่มอาหารสำเร็จ');
+                    stateComplete(true);
+                  }
                 }
               }
             >Confirm</button>
           </div>
         </div>
       </div>
+      {/* Popup ใส่ข้อมูลไม่ครบ */}
+      <Modal
+        visible={state_error}
+        closable={false}
+        footer={
+          <div className="w-full flex flex-col text-center items-center justify-center">
+            <Link className="w-full" to="Food">
+            <button className="font-prompt bg-blue-600 hover:bg-blue-700 w-1/3 text-white py-2 text-base rounded shadow-lg" onClick={()=>stateComplete(false)}>Cancel</button>
+            </Link>
+          </div>
+        }
+      >
+        <div className="w-full flex flex-col text-center items-center justify-center">
+          <img src="/pic/image_23.png" alt="check" className="h-20 w-20 m-10 "/>
+          <h3 className="text-3xl font-semibold font-prompt flex-auto">เพิ่มอาหารสำเร็จ</h3>
+        </div>
+      </Modal>
       {/* Popup สร้างอาหารสำเร็จ */}
       <Modal
         visible={state}
